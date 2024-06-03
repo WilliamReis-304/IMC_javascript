52391acd5e816aab77eee19e7ebdc007e2c0e5ff
Aluno da Infinity School <aluno@3D.local>
2024-06-03 19:28:55 -0300

    version 1.0.1

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 let nome = prompt("Insira o seu nome:")
-let altura = Number(prompt("Insira a sua altura em centímetros:"))
+let alturaCent = Number(prompt("Insira a sua altura EM CENTÍMETROS:"))
 let peso = Number(prompt("Insira seu peso:"))
 
-const imc = peso/(altura**2)
-console.log(imc)
+let alturaMetros = alturaCent/100
 
-if(imc>16){
+let imc = peso/(alturaMetros**2)
+console.log(`O paciente ${nome} tem o IMC = ${imc} e sua classificação é:`)
+
+if(imc<16){
     console.log("Baixo peso muito grave")
-}else if(imc<=16 & imc>17){
+}else if(imc>=16 & imc<17){
     console.log("Baixo peso grave")
-}else if(imc<=17 & imc>18.5){
+}else if(imc>=17 & imc<18.5){
     console.log("Baixo peso")
-}else if(imc<=18.5 & imc>25){
+}else if(imc>=18.5 & imc<25){
     console.log("Peso normal")
-}else if(imc<=25 & imc>30){
+}else if(imc>=25 & imc<30){
     console.log("Sobrepeso")
-}else if(imc<=30 & imc>35){
+}else if(imc>=30 & imc<35){
     console.log("Obesidade grau I")
-}else if(imc<=35 & imc>40){
+}else if(imc>=35 & imc<40){
     console.log("Obesidade grau II")
-}else{
+}else if(imc>=40){
     console.log("Obesidade grau III")
 }
